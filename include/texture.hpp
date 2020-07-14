293770904936aaff8ecfaa6921c3560ecaa81e0e
whichxjy <whichxjy@gmail.com>
2020-07-14 11:34:40 +0800

    Implement checker texture

--- include/texture.hpp
+++ include/texture.hpp
@@ -10,7 +10,6 @@ public:
 
 class SolidColor : public Texture {
 public:
-    SolidColor() {}
     SolidColor(Vec3 color) : color(color) {}
 
     SolidColor(double r, double g, double b) : SolidColor(Vec3(r, g, b)) {}
@@ -21,6 +20,27 @@ public:
 
 private:
     Vec3 color;
+};
+
+class CheckerTexture : public Texture {
+public:
+    CheckerTexture() {}
+    CheckerTexture(std::shared_ptr<Texture> lhs, std::shared_ptr<Texture> rhs)
+        : even(lhs), odd(rhs) {}
+
+    virtual Vec3 value(double u, double v, const Vec3& p) const {
+        auto sines = sin(10 * p.x()) * sin(10 * p.y()) * sin(10 * p.z());
+
+        if (sines < 0) {
+            return odd->value(u, v, p);
+        }
+
+        return even->value(u, v, p);
+    }
+
+private:
+    std::shared_ptr<Texture> odd;
+    std::shared_ptr<Texture> even;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,10 @@
 HittableList get_scene() {
     HittableList world;
 
-    auto ground_material = std::make_shared<Lambertian>(
-        std::make_shared<SolidColor>(0.3, 0.4, 0.5));
+    auto checker = std::make_shared<CheckerTexture>(
+        std::make_shared<SolidColor>(0.2, 0.3, 0.1),
+        std::make_shared<SolidColor>(0.9, 0.9, 0.9));
+    auto ground_material = std::make_shared<Lambertian>(checker);
     world.add(
         std::make_shared<Sphere>(Vec3(0, -1000, 0), 1000, ground_material));
 
